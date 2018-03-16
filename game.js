3b12fb56ba84641eb632d5c3ff93a528db250b08
rhasrie82 <33564425+rhasrie82@users.noreply.github.com>
2018-03-15 22:35:31 -0400

    I've added the files!

---
+++ game.js
@@ -0,0 +1,54 @@
+
+		
+		 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+
+		 var wins = 0;
+		 var losses = 0;
+		 var numberofGuesses = 9;
+		 var guessChoices = [];
+
+		 document.onkeyup = function(event) {
+
+		 	var userGuess = event.key;
+
+		 	var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+		 	var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+		 	
+
+			if (options.indexOf(userGuess) > -1) {
+
+				if (userGuess === computerGuess) {
+					wins++;
+					numberofGuesses = 9;
+					guessChoices = [];
+				}
+
+				if (userGuess != computerGuess) {
+					numberofGuesses --;
+					guessChoices.push(userGuess);
+				}
+
+				if (numberofGuesses === 0) {
+
+				numberofGuesses = 9;
+				losses ++;
+				guessChoices = [];
+
+				
+			}
+
+			var html = 
+			"<h1> The Psychic Game </h1>" +
+			"<p>Guess what letter I'm thinking of!</p>" +
+			"<p>Wins: " + wins + "</p>" +
+			"<p>Losses: " + losses + "</p>" +
+			"<p>Guesses Left: " + numberofGuesses + "</p>" +
+			"<p>Your Guesses so far: " + guessChoices.join(", ") + "</p>";
+
+			document.querySelector("#game").innerHTML = html;
+
+			
+			}
+		};
+
